083049e58ed4e0304eecec1ecf0f18477b4f31d8
pado140 <pado140@gmail.com>
2023-05-23 09:55:07 -0400

    Add git

---
+++ src/test/java/com/c3editions/app/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c3editions.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
